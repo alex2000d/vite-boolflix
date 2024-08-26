ae0bfd32a87f7056b27dec0342f318de9d9f09d3
alex2000d <demarcoalessandro777@gmail.com>
2024-08-26 16:12:24 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import Appheader from './components/Appheader.vue';
+export default {
+  components:{
+    Appheader,
+    
+  }
+}
+</script>
+<template>
+  <div>
+    
+  </div>
+</template>
+<style lang="scss">
+@use './style/generals.scss'
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    
+}
+</script>
+<template>
+   <div>
+     <input type="search">
+   </div>
+</template>
+<style lang="scss" scoped>
+   
+</style>
