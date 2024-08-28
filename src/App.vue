255e20815af42ff299fe7b8247c90d16736a4a4f
alex2000d <demarcoalessandro777@gmail.com>
2024-08-28 12:27:21 +0200

    [UPD] added the section content for films and series

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script>
 import { store } from './store'
 import AppHeader from './components/AppHeader.vue';
-
+import AppContent from './components/AppContent.vue';
 export default {
   components:{
     AppHeader,
+    AppContent
   },
   data(){
     return{
@@ -16,7 +17,7 @@ export default {
 <template lang="">
 <div>
 <AppHeader></AppHeader>
-
+<AppContent></AppContent>
 </div>
 </template>
 <style lang="scss">
---
+++ src/components/AppContent.vue
@@ -0,0 +1,66 @@
+<script>
+import { store } from '../store';
+import axios from 'axios'
+export default {
+  components:{
+    
+    axios
+  },
+  data(){
+    return{
+        store,
+        
+    }
+  },
+  
+}
+</script>
+<template lang="">
+<div class="container-content my-4">
+
+   <h1>I FILM DI NETFLIX</h1>
+  <div class="movie-container">
+      <AppFilm v-for="(item, index) in store.movieList" :key="index" :movie="item"></AppFilm>
+  </div>
+  <h1>LE SERIE TV DI NETFLIX</h1>
+  <div class="movie-container">
+      <AppSeries v-for="(item, index) in store.serieList" :key="index" :cardSerie="item"></AppSeries>
+  
+  </div>
+
+</div>
+
+</template>
+<style lang="scss">
+
+.container-content{
+  background-color: white;
+  width: 100%;
+
+  h1{
+    color: black;
+    margin-left: 1rem;
+
+  }
+
+  .movie-container{
+    width: 100%;
+    display: flex;
+    justify-content:space-between;
+    height: 100%;
+    overflow-x: scroll;
+    
+  }
+  ::-webkit-scrollbar{
+  width:0.8em;
+  height:0.8em;
+  background: white;
+  margin-right: 10px;
+}
+::-webkit-scrollbar-thumb{
+  background-color:gray ;
+  border: 1px solid black;
+}
+  
+}
+</style>
